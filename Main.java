cab70a8c338b726550bae44ef9bb37d8074426e9
jivyyyxd <hanifardhito2008@gmail.com>
2024-02-01 08:46:37 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main extends Person{
+    public static void main(String[] args) {
+        Person mPerson = new Person();
+        mPerson.setName("NEPP");
+    
+        System.out.println("Person Name: " + mPerson.getName());
+    }
+
+}
